c0a3ed2e3387c69dbb6bd0448370015f385103f7
kanakgarg007 <105218874+kanakgarg007@users.noreply.github.com>
2022-05-11 16:33:38 +0530

    Update newscript.js

--- newscript.js
+++ newscript.js
@@ -1,6 +1,6 @@
 // api url
 const api_url =
-	"https://ec2-13-233-91-89.ap-south-1.compute.amazonaws.com:8080/search";
+	"http://ec2-13-233-91-89.ap-south-1.compute.amazonaws.com:8080/search";
 
 // Defining async function
 async function getapi(url) {
@@ -26,38 +26,21 @@ function hideloader() {
 // Function to define innerHTML for HTML table
 function show(data) {
 	let tab =
-		'<tr>
-		<th>Id</th>
-		<th>Code</th>
-		<th>Name</th>
-		<th>Access URL</th>
-                <th>Account Type</th>
-                <th>Status Code</th>
-                <th>Source</th>
-                <th>GoLive</th>
-                <th>Bussiness Name</th>
-                <th>Bussiness Model</th>
-                <th>Industry</th>
-                <th>Created</th>
-                <th>Updated</th>
-		</tr>';
+		`<tr>
+		<th>id</th>
+		<th>code</th>
+		<th>name</th>
+		<th>accessUrl</th>
+		</tr>`;
 	
 	// Loop to access all rows
 	for (let r of data.list) {
-		tab += '<tr>
+		tab += `<tr>
 	<td>${r.id} </td>
+	<td>${r.code}</td>
 	<td>${r.name}</td>
-	<td>${r.accessUrl}</td>
-	<td>${r.accountType}</td>	
-	<td>${r.statusCode} </td>
-	<td>${r.source}</td>
-	<td>${r.goLive}</td>
-	<td>${r.bussinessName}</td>
-	<td>${r.bussinessModel} </td>
-	<td>${r.industry}</td>
-	<td>${r.created}</td>
-	<td>${r.updated}</td>		
-</tr>';
+	<td>${r.accessUrl}</td>		
+</tr>`;
 	}
 	// Setting innerHTML as tab variable
 	document.getElementById("employees").innerHTML = tab;
